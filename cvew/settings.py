1630c497bd8ac7380dcbc01000eac1530b136675
masozee <nurojilukmansyah@gmail.com>
2022-06-07 10:08:56 +0700

    add decoupled file

--- cvew/settings.py
+++ cvew/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r_ol16fry%&ye+#%#w7dlcl*+j(cq_%_8q4lyg!bkf_rrhlpvv'
+SECRET_KEY = config('PROD_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
